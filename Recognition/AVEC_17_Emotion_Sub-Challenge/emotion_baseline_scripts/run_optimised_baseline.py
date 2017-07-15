6c5dbf09c9ec04af5e5060edacd1e817b497b108
jkim <batiki09@gmail.com>
2017-07-15 17:29:29 +0200

    updated for more functions

--- Recognition/AVEC_17_Emotion_Sub-Challenge/emotion_baseline_scripts/run_optimised_baseline.py
+++ Recognition/AVEC_17_Emotion_Sub-Challenge/emotion_baseline_scripts/run_optimised_baseline.py
@@ -6,16 +6,16 @@ import os
 print("Baseline optimised for arousal:")
 delay      = 2.2
 # text only
-os.system("python ./emotion_baseline_scripts/run_baseline.py -delay " + str(delay) + " --text")
+os.system("python ./emotion_baseline_scripts/run_baseline.py --arousal --valence --liking -delay " + str(delay) + " --text")
 print(" ")
 
 print("Baseline optimised for valence:")
 delay      = 1.2
 # multimodal
-os.system("python ./emotion_baseline_scripts/run_baseline.py -delay " + str(delay) + " --audio --video --text")
+os.system("python ./emotion_baseline_scripts/run_baseline.py --arousal --valence --liking -delay " + str(delay) + " --audio --video --text")
 print(" ")
 
 print("Baseline optimised for liking:")
 delay      = 2.0
 # text only
-os.system("python ./emotion_baseline_scripts/run_baseline.py -delay " + str(delay) + " --text")
+os.system("python ./emotion_baseline_scripts/run_baseline.py --arousal --valence --liking -delay " + str(delay) + " --text")
