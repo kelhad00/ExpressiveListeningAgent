6ea6b015b8361dba915f9be5e12d3060ef62559b
TrungNT <nickartin13@gmail.com>
2017-07-17 16:49:36 +0100

    Add expression

--- SynthesisModule/test.py
+++ SynthesisModule/test.py
@@ -5,10 +5,11 @@ import numpy as np
 
 from utils import Audio, Video, read_openface
 # from face3danim import get3d_data
-from expression import Expression, Happy, Sad
+from expression import Happy, Sad, Laugh
 
-happy = Happy(1, 77)
+happy = Happy(1, 76)
 sad = Sad(1, 96)
+laugh = Laugh(2, 300)
 last_exp = sad
 
 
@@ -18,7 +19,8 @@ last_exp = sad
 # ===========================================================================
 def logic(video):
     # you should see shit continuosly appear on screen
-    print("Shit")
+    # print("Shit", video.last_name)
+    pass
 
 
 def callback(video):
@@ -28,10 +30,10 @@ def callback(video):
     last_exp = happy if last_exp == sad else happy
     video.play_frames(frames, queue=True)
     video.audio.play("data/n9.wav")
-    print("END")
+    # print("END")
 
 b = Video()
-b.play_frames(happy.frames)
+b.play_expression(laugh)
 b.set_callback(end_frames=callback, logic_processing=logic)
 b.run()
 # b.terminate()
